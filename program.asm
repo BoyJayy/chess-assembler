ae213101c1ee4b16a875e89e32cc2a86a3759f9e
BoyJayy <popkov.ad@icloud.com>
2025-11-24 01:43:02 +0300

    Update program.asm

--- program.asm
+++ program.asm
@@ -5,9 +5,7 @@ code segment
     assume cs: code
 
 start:
-    ; Инициализация режима отображения
     int 10h
-    ; Инициализация шахматного поля
     mov si, offset chessboard_data
     mov di, offset chessboard
 
@@ -16,44 +14,39 @@ start:
 
 main_loop:
     ; Основной цикл игры
-    call display_chessboard  ; Отображение доски
-    call player_move         ; Ход игрока
-    call update_chessboard   ; Обновление доски
-    jmp main_loop            ; Повторить цикл
+    call display_chessboard 
+    call player_move       
+    call update_chessboard 
+    jmp main_loop          
 
-; Инициализация шахматной доски
 init_chessboard:
-    ; Заполняем шахматную доску начальной расстановкой
-    mov cx, 8               ; 8 строк
-    mov bx, 0               ; Начальная строка
+    mov cx, 8              
+    mov bx, 0              
 fill_board:
     ; Заполняем клетки с фигурами
-    mov al, [si]            ; Считываем текущий байт данных (позиции фигур)
-    mov [di], al            ; Записываем в игровое поле
+    mov al, [si]         
+    mov [di], al         
     inc si
     inc di
     loop fill_board
     ret
 
-; Отображение шахматной доски
 display_chessboard:
     mov si, offset chessboard
-    mov cx, 64              ; 8x8 доска (64 клетки)
-    mov dl, 0Ah             ; Символ новой строки для отображения
+    mov cx, 64            
+    mov dl, 0Ah            
 display_loop:
-    lodsb                   ; Чтение байта из доски
-    cmp al, 20h             ; Проверка на пустую клетку (символ пробела)
+    lodsb               
+    cmp al, 20h         
     jz display_empty_square
-    ; Если клетка не пуста, отобразить символ фигуры
     mov ah, 02h
-    int 21h                 ; Вывод символа
+    int 21h             
     jmp display_continue
 
 display_empty_square:
-    ; Если клетка пуста, вывести точку
     mov dl, '.'
     mov ah, 02h
-    int 21h                 ; Вывод символа
+    int 21h          
 
 display_continue:
     loop display_loop
@@ -62,63 +55,52 @@ display_continue:
     int 21h
     ret
 
-; Ход игрока
 player_move:
-    ; Ожидаем ввод хода игрока в формате "e2 e4"
-    ; Первая буква - колонка (a-h), вторая - строка (1-8)
-    ; Преобразуем координаты из символов в индексы массива (0-63)
-    mov ah, 01h             ; Ввод символа
-    int 21h                 ; Чтение первого символа (например, 'e')
-    sub al, 61h             ; Преобразуем в индекс (0 = a, 1 = b, ..., 7 = h)
-    mov bl, al              ; Сохраняем в BL (для колонок)
+    mov ah, 01h        
+    int 21h            
+    sub al, 61h       
+    mov bl, al           
 
-    mov ah, 01h             ; Ввод второго символа (например, '2')
+    mov ah, 01h           
     int 21h
-    sub al, 30h             ; Преобразуем в число (1 = 1, 2 = 2, ..., 8 = 8)
-    sub al, 1               ; Переводим в индекс (0 = 1, 1 = 2, ..., 7 = 8)
-    mov bh, al              ; Сохраняем строку в BH
+    sub al, 30h          
+    sub al, 1            
+    mov bh, al           
 
-    ; Чтение пробела между координатами
     mov ah, 01h
     int 21h
 
-    ; Повторяем для второй клетки (например, "e4")
     mov ah, 01h
     int 21h
     sub al, 61h
-    mov bl, al              ; Сохраняем в BL (для колонок)
+    mov bl, al          
 
     mov ah, 01h
     int 21h
     sub al, 30h
     sub al, 1
-    mov bh, al              ; Сохраняем строку в BH
+    mov bh, al             
 
-    ; Обновляем шахматную доску
-    ; Перемещение фигуры на доске
     mov si, offset chessboard
     mov di, si
-    add di, bx              ; Перемещение в колонку
-    add di, 8 * bh          ; Перемещение в строку
-    mov al, [si]            ; Чтение фигуры
-    mov [di], al            ; Перемещение фигуры
-    mov byte ptr [si], 20h  ; Очистка исходной клетки
+    add di, bx           
+    add di, 8 * bh        
+    mov al, [si]           
+    mov [di], al         
+    mov byte ptr [si], 20h 
 
     ret
 
-; Обновление доски после хода
 update_chessboard:
-    ; Логика обновления доски после хода (пока нет)
     ret
-
-; Данные шахматной доски (инициализация позиций фигур)
 chessboard_data db 
-    10h, 12h, 14h, 16h, 18h, 14h, 12h, 10h,  ; Черные фигуры
-    20h, 20h, 20h, 20h, 20h, 20h, 20h, 20h,  ; Пустые клетки
-    30h, 30h, 30h, 30h, 30h, 30h, 30h, 30h,  ; Белые пешки
-    10h, 12h, 14h, 16h, 18h, 14h, 12h, 10h,  ; Белые фигуры
+    10h, 12h, 14h, 16h, 18h, 14h, 12h, 10h,  
+    20h, 20h, 20h, 20h, 20h, 20h, 20h, 20h, 
+    30h, 30h, 30h, 30h, 30h, 30h, 30h, 30h, 
+    10h, 12h, 14h, 16h, 18h, 14h, 12h, 10h,  
 
-chessboard db 64 dup(20h)  ; Место для самой шахматной доски (символы фигур или пустые клетки)
+chessboard db 64 dup(20h) 
 
 code ends
 end start
+
