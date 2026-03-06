a50ba0223779196526ee77d784709c6fc18e28b5
BoyJayy <popkov.ad@icloud.com>
2026-03-06 16:28:19 +0300

    Update program.asm

--- program.asm
+++ program.asm
@@ -1,102 +1,136 @@
 .model tiny
 .186
 code segment
-    org 100h
-    assume cs: code
+org 100h
+assume cs:code, ds:code
 
 start:
-    int 10h
+    push cs
+    pop ds
+
     mov si, offset chessboard_data
     mov di, offset chessboard
     call init_chessboard
 
 main_loop:
-    call display_chessboard 
-    call player_move       
-    call update_chessboard 
-    jmp main_loop          
+    call display_chessboard
+    call player_move
+    jmp main_loop
 
 init_chessboard:
-    mov cx, 8              
-    mov bx, 0              
-fill_board:
-    mov al, [si]         
-    mov [di], al         
+    mov cx, 64
+copy_loop:
+    mov al, [si]
+    mov [di], al
     inc si
     inc di
-    loop fill_board
+    loop copy_loop
     ret
 
 display_chessboard:
     mov si, offset chessboard
-    mov cx, 64            
-    mov dl, 0Ah            
-display_loop:
-    lodsb               
-    cmp al, 20h         
-    jz display_empty_square
-    mov ah, 02h
-    int 21h             
-    jmp display_continue
+    mov cx, 64
+    xor bx, bx
 
-display_empty_square:
+print_loop:
+    lodsb
+    cmp al, ' '
+    jne print_piece
     mov dl, '.'
-    mov ah, 02h
-    int 21h          
+    jmp print_char
 
-display_continue:
-    loop display_loop
+print_piece:
+    mov dl, al
+
+print_char:
     mov ah, 02h
     int 21h
+
+    mov dl, ' '
+    mov ah, 02h
+    int 21h
+
+    inc bl
+    cmp bl, 8
+    jne no_newline
+
+    mov dl, 13
+    mov ah, 02h
+    int 21h
+    mov dl, 10
+    mov ah, 02h
+    int 21h
+    xor bl, bl
+
+no_newline:
+    loop print_loop
     ret
 
 player_move:
-    mov ah, 01h        
-    int 21h            
-    sub al, 61h       
-    mov bl, al           
-
-    mov ah, 01h           
+    mov ah, 01h
     int 21h
-    sub al, 30h          
-    sub al, 1            
-    mov bh, al           
+    sub al, 'a'
+    mov dl, al
 
     mov ah, 01h
     int 21h
+    sub al, '1'
+    mov dh, al
 
     mov ah, 01h
     int 21h
-    sub al, 61h
-    mov bl, al          
+    sub al, 'a'
+    mov bl, al
 
     mov ah, 01h
     int 21h
-    sub al, 30h
-    sub al, 1
-    mov bh, al             
+    sub al, '1'
+    mov bh, al
 
+    xor ax, ax
+    mov al, dh
+    shl al, 1
+    shl al, 1
+    shl al, 1
+    add al, dl
+    xor si, si
     mov si, offset chessboard
-    mov di, si
-    add di, bx           
-    add di, 8 * bh        
-    mov al, [si]           
-    mov [di], al         
-    mov byte ptr [si], 20h 
+    add si, ax
+
+    xor ax, ax
+    mov al, bh
+    shl al, 1
+    shl al, 1
+    shl al, 1
+    add al, bl
+    xor di, di
+    mov di, offset chessboard
+    add di, ax
+
+    mov al, [si]
+    mov [di], al
+    mov byte ptr [si], ' '
+
+    mov dl, 13
+    mov ah, 02h
+    int 21h
+    mov dl, 10
+    mov ah, 02h
+    int 21h
 
     ret
 
-update_chessboard:
-    ret
-chessboard_data db 
-    10h, 12h, 14h, 16h, 18h, 14h, 12h, 10h,  
-    20h, 20h, 20h, 20h, 20h, 20h, 20h, 20h, 
-    30h, 30h, 30h, 30h, 30h, 30h, 30h, 30h, 
-    10h, 12h, 14h, 16h, 18h, 14h, 12h, 10h,  
+chessboard_data db \
+'R','N','B','Q','K','B','N','R',\
+'P','P','P','P','P','P','P','P',\
+' ',' ',' ',' ',' ',' ',' ',' ',\
+' ',' ',' ',' ',' ',' ',' ',' ',\
+' ',' ',' ',' ',' ',' ',' ',' ',\
+' ',' ',' ',' ',' ',' ',' ',' ',\
+'p','p','p','p','p','p','p','p',\
+'r','n','b','q','k','b','n','r'
 
-chessboard db 64 dup(20h) 
+chessboard db 64 dup(' ')
 
 code ends
 end start
-
-
